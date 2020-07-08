d80ded345188b3c27ac6681ac366ffd6dcf38717
JancyRoachR <jancy.roach@htcindia.com>
2020-07-08 19:28:32 +0530

    Added loggers and NyplOauthTokenApiUrl

--- src/main/java/org/recap/ils/service/NyplOauthTokenApiService.java
+++ src/main/java/org/recap/ils/service/NyplOauthTokenApiService.java
@@ -1,6 +1,9 @@
 package org.recap.ils.service;
 
+
 import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -18,6 +21,8 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class NyplOauthTokenApiService {
 
+    private static final Logger logger = LoggerFactory.getLogger(NyplOauthTokenApiService.class);
+
     @Value("${ils.nypl.oauth.token.api}")
     public String nyplOauthTokenApiUrl;
 
@@ -29,6 +34,7 @@ public class NyplOauthTokenApiService {
 
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> requestEntity = new HttpEntity("grant_type=client_credentials", headers);
+        logger.error("NyplOAuthToken"+nyplOauthTokenApiUrl);
         ResponseEntity<String> responseEntity = restTemplate.exchange(nyplOauthTokenApiUrl, HttpMethod.POST, requestEntity, String.class);
         JSONObject jsonObject = new JSONObject(responseEntity.getBody());
         return (String) jsonObject.get("access_token");
