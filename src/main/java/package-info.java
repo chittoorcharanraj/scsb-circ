6a1eebd513a98a2a703efb27f3aca4ec091b441f
ndinakar <dinkarn31@gmail.com>
2022-11-11 11:04:09 +0530

    adding ns1 prefix

---
+++ src/main/java/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Dinakar N created on 11/11/22
+ */
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.niso.org/2008/ncip", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = { @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.niso.org/2008/ncip", prefix = "ns1") })
