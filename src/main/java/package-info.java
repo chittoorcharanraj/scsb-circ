7fffced7a4c170bf14fd26d3a0ae23204296cde9
ndinakar <dinkarn31@gmail.com>
2022-11-11 15:11:23 +0530

    changes for ncip

--- src/main/java/package-info.java
+++ src/main/java/package-info.java
@@ -2,4 +2,8 @@
  * @author Dinakar N created on 11/11/22
  */
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.niso.org/2008/ncip", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = { @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.niso.org/2008/ncip", prefix = "ns2") })
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+
+/*@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.niso.org/2008/ncip", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = { @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.niso.org/2008/ncip", prefix = "ns2") })*/
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.niso.org/2008/ncip", xmlns = {@XmlNs(prefix = "ns2", namespaceURI = "http://www.niso.org/2008/ncip")}, elementFormDefault = XmlNsForm.QUALIFIED)
